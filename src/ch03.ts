c7fb1717c0841878a722c07d627ca57845fc4694
Donghee Kim <dongheekim@woowahan.com>
2022-05-05 21:48:11 +0900

    feat: 연습문제 3.3을 푼다

--- src/ch03.ts
+++ src/ch03.ts
@@ -66,3 +66,27 @@ export function sift(arr: boolean[]): void {
     index_square += factor;
   }
 }
+
+const value = (i: number): number => 2 * i + 3;
+const index = (v: number): number => Math.floor((v - 3) / 2);
+const prime_numbers = (arr: boolean[]): number[] =>
+  [...arr.entries()].filter(([, v]) => v).map(([i]) => value(i));
+
+const pi = (n: number): number => {
+  if (n <= 1) return 0;
+  if (n <= 4) return 1;
+
+  const arr = Array(index(n));
+  sift(arr);
+
+  return prime_numbers(arr).length + 1;
+};
+
+const max = 107;
+let sum = 0;
+for (let i = 1; i <= 107; i++) {
+  sum += Math.abs(pi(i) - (Math.floor(i / 4) + 2)) ** 2;
+}
+const average = sum / max;
+
+console.log(average);
