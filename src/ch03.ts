ed13f61fe6cd8e3e084afdb8599b82e1bc567393
Donghee Kim <dongheekim@woowahan.com>
2022-05-05 21:04:59 +0900

    feat: sift0을 구현한다

--- src/ch03.ts
+++ src/ch03.ts
@@ -14,3 +14,18 @@ export function mark_sieve(
     arr[first] = false;
   }
 }
+
+export function sift0(arr: boolean[], n: number): void {
+  arr.fill(true);
+
+  let i = 0;
+  let index_square = 3;
+
+  while (index_square < n) {
+    if (arr[i]) {
+      mark_sieve(arr, index_square, n, i + i + 3);
+    }
+    ++i;
+    index_square = 2 * i * (i + 3) + 3;
+  }
+}
