7d23fdf3259c56c3ee6820009454d0507bfb79e9
Donghee Kim <dongheekim@woowahan.com>
2022-05-19 20:47:39 +0900

    feat: 4장 fast_segment_remainder을 구현한다

--- src/ch04.ts
+++ src/ch04.ts
@@ -51,3 +51,29 @@ assert.equal(gcm(42, 196), 14);
 assert.equal(gcm(45, 6), 3);
 assert.equal(gcm(3, 2), 1);
 assert.equal(gcm(3, 1), 1);
+
+function fast_segment_remainder(
+  a: line_segment,
+  b: line_segment
+): line_segment {
+  if (a <= b) return a;
+  if (a - b <= b) return a - b;
+  a = fast_segment_remainder(a, b + b);
+  if (a <= b) return a;
+  return a - b;
+}
+
+function fast_segment_gcm(a: line_segment, b: line_segment): line_segment {
+  while (a != b) {
+    a = fast_segment_remainder(a, b);
+    [a, b] = [b, a];
+  }
+
+  return a;
+}
+
+assert.equal(fast_segment_gcm(196, 42), 14);
+assert.equal(fast_segment_gcm(42, 196), 14);
+assert.equal(fast_segment_gcm(45, 6), 3);
+assert.equal(fast_segment_gcm(3, 2), 1);
+assert.equal(fast_segment_gcm(3, 1), 1);
