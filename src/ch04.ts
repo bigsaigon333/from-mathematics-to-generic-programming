2e5e875c13993116c62d843c7c49dc25597a8899
Donghee Kim <dongheekim@woowahan.com>
2022-05-19 20:36:56 +0900

    feat: 4장 gcm1을 구현한다

--- src/ch04.ts
+++ src/ch04.ts
@@ -16,3 +16,18 @@ assert.equal(gcm0(42, 196), 14);
 assert.equal(gcm0(45, 6), 3);
 assert.equal(gcm0(3, 2), 1);
 assert.equal(gcm0(3, 1), 1);
+
+function gcm1(a: line_segment, b: line_segment): line_segment {
+  while (a != b) {
+    while (b < a) a = a - b;
+    [a, b] = [b, a];
+  }
+
+  return a;
+}
+
+assert.equal(gcm1(196, 42), 14);
+assert.equal(gcm1(42, 196), 14);
+assert.equal(gcm1(45, 6), 3);
+assert.equal(gcm1(3, 2), 1);
+assert.equal(gcm1(3, 1), 1);
