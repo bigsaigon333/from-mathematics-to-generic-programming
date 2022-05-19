4d0ec6b97b744beb9c6e249c4c3527acad1aa560
Donghee Kim <dongheekim@woowahan.com>
2022-05-19 21:00:13 +0900

    feat: 4장 remainder, quotient 을 구현한다

--- src/ch04.ts
+++ src/ch04.ts
@@ -77,3 +77,59 @@ assert.equal(fast_segment_gcm(42, 196), 14);
 assert.equal(fast_segment_gcm(45, 6), 3);
 assert.equal(fast_segment_gcm(3, 2), 1);
 assert.equal(fast_segment_gcm(3, 1), 1);
+
+function fast_segment_remainder1(
+  a: line_segment,
+  b: line_segment
+): line_segment {
+  assert(b != 0);
+
+  if (a < b) return a;
+  if (a - b < b) return a - b;
+  a = fast_segment_remainder1(a, b + b);
+  if (a < b) return a;
+  return a - b;
+}
+
+function largest_doubling(a: line_segment, b: line_segment): line_segment {
+  assert(b != 0);
+
+  while (a - b >= b) b = b + b;
+  return b;
+}
+
+function half(c: line_segment): line_segment {
+  return c >> 1;
+}
+
+function remainder(a: line_segment, b: line_segment): line_segment {
+  assert(b != 0);
+
+  if (a < b) return a;
+  let c: line_segment = largest_doubling(a, b);
+  a = a - c;
+  while (c != b) {
+    c = half(c);
+    if (c <= a) a = a - c;
+  }
+  return a;
+}
+
+function quotient(a: line_segment, b: line_segment): number {
+  assert(b > 0);
+
+  if (a < b) return 0;
+  let c: line_segment = largest_doubling(a, b);
+  let n = 1;
+  a = a - c;
+  while (c != b) {
+    c = half(c);
+    n = n + n;
+    if (c <= a) {
+      a = a - c;
+      n = n + 1;
+    }
+  }
+
+  return n;
+}
