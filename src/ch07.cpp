aecf2f44b29b4922b116dd24b56cffb23e0878d8
Donghee Kim <dongheekim@woowahan.com>
2022-06-30 22:00:35 +0900

    feat: 7.3 N에 대한 요구조건

--- src/ch07.cpp
+++ src/ch07.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
-int odd(int n) {
-  return n & 0x01;
+#define NoncommutativeAdditiveSemigroup typename
+#define Integer typename
+
+template<Integer N>
+bool odd(N n) {
+  return bool(n & 0x01);
 }
 
-int half(int n ) {
+template<Integer N>
+N half(N n) {
   return n >> 1;
 }
 
@@ -31,14 +36,43 @@ A multiply_accumulate(A r, N n, A a) {
   }
 }
 
-\
 int multiply2(int n, int a) {
   if (n == 1) return a;
   return multiply_accumulate(a, n-1, a);
 }
 
+template <NoncommutativeAdditiveSemigroup A, typename N>
+A multiply_accumulate_semigroup(A r, N n, A a) {
+  // assert: n >= 0
+  if (n == 0)
+    return r;
+
+  while (true)
+  {
+    if (odd(n)) {
+      r = r + a;
+      if (n == 1) return r;
+    }
+    n = half(n);
+    a = a + a;
+  }
+}
+
+template <NoncommutativeAdditiveSemigroup A, Integer N>
+A multiply_semigroup(N n, A a) {
+  // assert: n > 0
+  while (!odd(n)) {
+    a = a + a;
+    n = half(n);
+  }
+
+  if (n == 1)
+    return a;
+  return multiply_accumulate_semigroup(a, half(n - 1), a + a);
+}
+
 int main() {
   int n, a;
   std::cin>>n>>a;
-  std::cout<<multiply2(n, a)<<std::endl;
+  std::cout<<multiply_semigroup(n, a)<<std::endl;
 }
