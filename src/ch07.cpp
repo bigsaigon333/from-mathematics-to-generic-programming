32e14c8fe4cc2884cd7f2fd295559b5e6b81e54f
Donghee Kim <dongheekim@woowahan.com>
2022-06-30 21:34:19 +0900

    feat: 7-1. 곱셈 누적 초기버전을 작성한다

---
+++ src/ch07.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int odd(int n) {
+  return n & 0x01;
+}
+
+int half(int n ) {
+  return n >> 1;
+}
+
+int mult_acc4(int r, int n, int a) {
+  while(true) {
+    if (odd(n)) {
+      r = r + a;
+      if (n == 1) return r;
+    }
+    n = half(n);
+    a = a + a;
+  }
+}
+
+int multiply2(int n, int a) {
+  if (n == 1) return a;
+  return mult_acc4(a, n-1, a);
+}
+
+int main() {
+  int n, a;
+  std::cin>>n>>a;
+  std::cout<<multiply2(n, a)<<std::endl;
+}
